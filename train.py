52a8dd4f2fea0b69f124bb3510a7319655253b8e
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-04 19:39:59 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,91 @@
+import torch
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+import time
+from tqdm import tqdm
+from conv import ConvNet4
+# from resnet import resnet12
+
+# Set device to GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Define transforms
+transform = transforms.Compose(
+    [transforms.Resize((224, 224)),  # 调整图像大小
+     transforms.ToTensor(),  # 转换为张量
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# Load data
+train_data = ImageFolder('caltech-101/train', transform=transform)
+val_data = ImageFolder('caltech-101/test', transform=transform)
+
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True, num_workers=2)
+val_loader = DataLoader(val_data, batch_size=64, shuffle=False, num_workers=2)
+
+# Load pre-trained ResNet-18 model
+model = ConvNet4(num_classes=len(train_data.classes)).to(device)
+# model = resnet12().to(device)
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60, 70], gamma=0.05)
+
+# Train the model
+num_epochs = 80
+best_acc = 0.0
+for epoch in range(num_epochs):
+    train_loss = 0.0
+    train_correct = 0
+    train_total = 0
+    val_loss = 0.0
+    val_correct = 0
+    val_total = 0
+    start_time = time.time()
+
+    # Train
+    model.train()
+    with tqdm(train_loader, unit="batch") as tepoch:
+        tepoch.set_description(f"Epoch {epoch+1}/{num_epochs}")
+        for inputs, labels in tepoch:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item() * inputs.size(0)
+            _, predicted = torch.max(outputs.data, 1)
+            train_total += labels.size(0)
+            train_correct += (predicted == labels).sum().item()
+            tepoch.set_postfix(loss=train_loss/train_total, acc=train_correct/train_total)
+
+    # Validate
+    model.eval()
+    with torch.no_grad():
+        with tqdm(val_loader, unit="batch") as tepoch:
+            tepoch.set_description(f"Epoch {epoch+1}/{num_epochs}")
+            for inputs, labels in tepoch:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item() * inputs.size(0)
+                _, predicted = torch.max(outputs.data, 1)
+                val_total += labels.size(0)
+                val_correct += (predicted == labels).sum().item()
+                tepoch.set_postfix(loss=val_loss/val_total, acc=val_correct/val_total)
+
+    lr_scheduler.step()
+    end_time = time.time()
+    print(f"Epoch {epoch+1}/{num_epochs} took {end_time - start_time:.2f} seconds")
+    print(f"Train loss: {train_loss/train_total:.4f}, Train accuracy: {train_correct/train_total:.4f}")
+    print(f"Validation loss: {val_loss/val_total:.4f}, Validation accuracy: {val_correct/val_total:.4f}")
+    # Update best accuracy
+    if val_correct / val_total > best_acc:
+        best_acc = val_correct / val_total
+
+print(f"Best validation accuracy: {best_acc:.4f}")
+
