c70469e2c1beef5010715cd699b087678f0f678e
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-06 17:15:08 +0800

    Update train.py

--- train.py
+++ train.py
@@ -21,8 +21,8 @@ transform = transforms.Compose(
 train_data = ImageFolder('caltech101/train', transform=transform)
 val_data = ImageFolder('caltech101/test', transform=transform)
 
-train_loader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=2)
-val_loader = DataLoader(val_data, batch_size=32, shuffle=False, num_workers=2)
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=4)
+val_loader = DataLoader(val_data, batch_size=32, shuffle=False, num_workers=4)
 
 
 model = resnet18gai().to(device)
