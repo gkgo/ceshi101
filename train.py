63303ec871c578e0b76a49b5ee0dfd106786c406
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-07 22:02:24 +0800

    Update train.py

--- train.py
+++ train.py
@@ -18,11 +18,11 @@ transform = transforms.Compose(
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 # Load data
-train_data = ImageFolder('caltech101/train', transform=transform)
-val_data = ImageFolder('caltech101/test', transform=transform)
+train_data = ImageFolder('data/caltech-101/train', transform=transform)
+val_data = ImageFolder('data/caltech-101/test', transform=transform)
 
-train_loader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=2)
-val_loader = DataLoader(val_data, batch_size=32, shuffle=False, num_workers=2)
+train_loader = DataLoader(train_data, batch_size=8, shuffle=True, num_workers=0)
+val_loader = DataLoader(val_data, batch_size=8, shuffle=False, num_workers=0)
 
 
 model = resnet18gai().to(device)
@@ -31,10 +31,10 @@ model = resnet18gai().to(device)
 # Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr =0.001,momentum=0.9,nesterov=True, weight_decay=0.0005)
-lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[80, 90], gamma=0.05)
+lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 40], gamma=0.05)
 
 # Train the model
-num_epochs = 100
+num_epochs = 50
 best_acc = 0.0
 for epoch in range(num_epochs):
     train_loss = 0.0
