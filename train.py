ec06c6def8deb3c50598588c92266787226d9f6b
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-07 22:13:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -18,8 +18,8 @@ transform = transforms.Compose(
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 # Load data
-train_data = ImageFolder('data/caltech-101/train', transform=transform)
-val_data = ImageFolder('data/caltech-101/test', transform=transform)
+train_data = ImageFolder('caltech-101/train', transform=transform)
+val_data = ImageFolder('caltech-101/test', transform=transform)
 
 train_loader = DataLoader(train_data, batch_size=8, shuffle=True, num_workers=0)
 val_loader = DataLoader(val_data, batch_size=8, shuffle=False, num_workers=0)
