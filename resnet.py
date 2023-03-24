bda164714352fd0733a71fb90efd3f407f73fcf0
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-24 19:37:48 +0800

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -1,294 +1,157 @@
-import torch.nn.functional as F
+from collections import OrderedDict
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-def featureL2Norm(feature):
-    epsilon = 1e-6
-    norm = torch.pow(torch.sum(torch.pow(feature, 2), 1) + epsilon, 0.5).unsqueeze(1).expand_as(feature)
-    return torch.div(feature, norm)
 
-class mySelfCorrelationComputation(nn.Module):
-    def __init__(self, kernel_size=(5, 5), padding=2):
-        super(mySelfCorrelationComputation, self).__init__()
-        planes =[512, 64, 64, 640]
-        self.kernel_size = kernel_size
-        self.unfold = nn.Unfold(kernel_size=kernel_size, padding=padding)
-        self.relu = nn.ReLU(inplace=False)
-
-        self.conv1x1_in = nn.Sequential(nn.Conv2d(planes[0], planes[1], kernel_size=1, bias=False, padding=0),
-                                        nn.BatchNorm2d(planes[1]),
-                                        nn.ReLU(inplace=True))
-        self.embeddingFea0 = nn.Sequential(nn.Conv2d(1664, 512,
-                                                     kernel_size=1, bias=False, padding=0),
-                                           nn.BatchNorm2d(512),
-                                           nn.ReLU(inplace=True))
-        self.conv1x1_out = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=1, bias=False, padding=0),
-            nn.BatchNorm2d(512))
+class BasicUnit(nn.Module):
+    def __init__(self, channels: int, dropout: float):
+        super(BasicUnit, self).__init__()
+        self.block = nn.Sequential(OrderedDict([
+            ("0_normalization", nn.BatchNorm2d(channels)),
+            ("1_activation", nn.ReLU(inplace=True)),
+            ("2_convolution", nn.Conv2d(channels, channels, (3, 3), stride=1, padding=1, bias=False)),
+            ("3_normalization", nn.BatchNorm2d(channels)),
+            ("4_activation", nn.ReLU(inplace=True)),
+            ("5_dropout", nn.Dropout(dropout, inplace=True)),
+            ("6_convolution", nn.Conv2d(channels, channels, (3, 3), stride=1, padding=1, bias=False)),
+        ]))
 
     def forward(self, x):
-
-        x = self.conv1x1_in(x)
-        b, c, h, w = x.shape
-
-        x0 = self.relu(x)
-        x = x0
-        x = F.normalize(x, dim=1, p=2)
-        identity = x
-
-        x = self.unfold(x)  # 提取出滑动的局部区域块，这里滑动窗口大小为5*5，步长为1
-        # b, cuv, h, w  （80,640*5*5,5,5)
-        x = x.view(b, c, self.kernel_size[0], self.kernel_size[1], h, w)  # b, c, u, v, h, w
-        x = x * identity.unsqueeze(2).unsqueeze(2)  # 通过unsqueeze增维使identity和x变为同维度  公式（1）
-        # b, c, u, v, h, w * b, c, 1, 1, h, w
-        x = x.view(b, -1, h, w)
-        # x = x.permute(0, 1, 4, 5, 2, 3).contiguous()  # b, c, h, w, u, v
-        # torch.contiguous()方法首先拷贝了一份张量在内存中的地址，然后将地址按照形状改变后的张量的语义进行排列
-        # x = x.mean(dim=[-1, -2])
-        feature_gs = featureL2Norm(x)
-
-        # concatenate
-        feature_cat = torch.cat([identity, feature_gs], 1)
-
-        # embed
-        feature_embd = self.embeddingFea(feature_cat)
-        feature_embd = self.conv1x1_out(feature_embd)
-        return feature_embd
+        return x + self.block(x)
 
 
-class SqueezeExcitation(nn.Module):
-    def __init__(self, channel, reduction=16):
-        super(SqueezeExcitation, self).__init__()
-        hdim = 64
-        self.conv1x1_in = nn.Sequential(nn.Conv2d(channel, hdim, kernel_size=1, bias=False, padding=0),
-                                        nn.BatchNorm2d(hdim),
-                                        nn.ReLU(inplace=False))
-        self.conv1x1_out = nn.Sequential(nn.Conv2d(hdim, channel, kernel_size=1, bias=False, padding=0),
-                                        nn.BatchNorm2d(channel),
-                                        nn.ReLU(inplace=False))
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(hdim, hdim // reduction, bias=False),
-            nn.ReLU(inplace=False),
-            nn.Linear(hdim // reduction, hdim, bias=False),
-            nn.Sigmoid()
+class DownsampleUnit(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, stride: int, dropout: float):
+        super(DownsampleUnit, self).__init__()
+        self.norm_act = nn.Sequential(OrderedDict([
+            ("0_normalization", nn.BatchNorm2d(in_channels)),
+            ("1_activation", nn.ReLU(inplace=True)),
+        ]))
+        self.block = nn.Sequential(OrderedDict([
+            ("0_convolution", nn.Conv2d(in_channels, out_channels, (3, 3), stride=stride, padding=1, bias=False)),
+            ("1_normalization", nn.BatchNorm2d(out_channels)),
+            ("2_activation", nn.ReLU(inplace=True)),
+            ("3_dropout", nn.Dropout(dropout, inplace=True)),
+            ("4_convolution", nn.Conv2d(out_channels, out_channels, (3, 3), stride=1, padding=1, bias=False)),
+        ]))
+        self.downsample = nn.Conv2d(in_channels, out_channels, (1, 1), stride=stride, padding=0, bias=False)
+
+    def forward(self, x):
+        x = self.norm_act(x)
+        return self.block(x) + self.downsample(x)
+
+
+class Block(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, stride: int, depth: int, dropout: float):
+        super(Block, self).__init__()
+        self.block = nn.Sequential(
+            DownsampleUnit(in_channels, out_channels, stride, dropout),
+            *(BasicUnit(out_channels, dropout) for _ in range(depth))
         )
 
     def forward(self, x):
-        x = self.conv1x1_in(x)
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        x = x * y.expand_as(x)
-        x = self.conv1x1_out(x)
-        return x
+        return self.block(x)
 
 
+class WideResNet(nn.Module):
+    def __init__(self, depth: int, width_factor: int, dropout: float, in_channels: int, labels: int):
+        super(WideResNet, self).__init__()
 
+        self.filters = [16, 1 * 16 * width_factor, 2 * 16 * width_factor, 4 * 16 * width_factor]
+        self.block_depth = (depth - 4) // (3 * 2)
 
+        self.f = nn.Sequential(OrderedDict([
+            ("0_convolution", nn.Conv2d(in_channels, self.filters[0], (3, 3), stride=1, padding=1, bias=False)),
+            ("1_block", Block(self.filters[0], self.filters[1], 1, self.block_depth, dropout)),
+            ("2_block", Block(self.filters[1], self.filters[2], 2, self.block_depth, dropout)),
+            ("3_block", Block(self.filters[2], self.filters[3], 2, self.block_depth, dropout)),
+            ("4_normalization", nn.BatchNorm2d(self.filters[3])),
+            ("5_activation", nn.ReLU(inplace=True)),
+            ("6_pooling", nn.AvgPool2d(kernel_size=8)),
+            ("7_flattening", nn.Flatten()),
+            ("8_classification", nn.Linear(in_features=self.filters[3], out_features=labels)),
+        ]))
 
+        self._initialize()
 
-
-class ChannelAttention(nn.Module):
-    def __init__(self, in_planes, ratio=8):
-        super(ChannelAttention, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-
-        # 利用1x1卷积代替全连接
-        self.fc1   = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
-        self.relu1 = nn.ReLU()
-        self.fc2   = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
-
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
-        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
-        out = avg_out + max_out
-        return self.sigmoid(out)
-
-class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=3):
-        super(SpatialAttention, self).__init__()
-
-        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
-        padding = 3 if kernel_size == 7 else 1
-        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avg_out = torch.mean(x, dim=1, keepdim=True)
-        max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
-        x = self.conv1(x)
-        return self.sigmoid(x)
-
-class cbam_block(nn.Module):
-    def __init__(self, channel, ratio=8, kernel_size=3):
-        super(cbam_block, self).__init__()
-        self.channelattention = ChannelAttention(channel, ratio=ratio)
-        self.spatialattention = SpatialAttention(kernel_size=kernel_size)
-
-    def forward(self, x):
-        x = x * self.channelattention(x)
-        x = x * self.spatialattention(x)
-        return x
-
-
-
-def conv3x3(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-
-def conv1x1(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-def normalize_feature(x):
-    return x - x.mean(1).unsqueeze(1)  # x-x.mean(1)行求平均值并在channal维上增加一个维度
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, *args, **kwargs):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        if inplanes != planes:
-            self.downsample = nn.Sequential(conv1x1(inplanes, planes, stride), nn.BatchNorm2d(planes))
-        else:
-            self.downsample = lambda x: x
-        self.stride = stride
-
-    def forward(self, x):
-        residual = self.downsample(x)
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = conv1x1(inplanes, planes)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = conv3x3(planes, planes, stride)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = conv1x1(planes, planes * self.expansion)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class ResNet(nn.Module):
-
-    def __init__(self, block, layers, num_classes=102, zero_init_residual=False):
-        super(ResNet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-#         self.scr_module = mySelfCorrelationComputation(kernel_size=(5, 5), padding=2)
-        self.scr_module = cbam_block(channel=512) 
-        self.fc = nn.Linear(512, num_classes)
-
+    def _initialize(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.kaiming_normal_(m.weight.data, mode="fan_in", nonlinearity="relu")
+                if m.bias is not None:
+                    m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = stride != 1
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, 1))
-
-        return nn.Sequential(*layers)
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.zero_()
+                m.bias.data.zero_()
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+        return self.f(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+    import torch
 
 
-        identity = x
-        x = self.scr_module(x)
+class SAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.05, adaptive=False, **kwargs):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
 
+        defaults = dict(rho=rho, adaptive=adaptive, **kwargs)
+        super(SAM, self).__init__(params, defaults)
 
-        x = x + identity
-#         x = F.relu(x, inplace=True)
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+        self.defaults.update(self.base_optimizer.defaults)
 
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-12)
 
+            for p in group["params"]:
+                if p.grad is None: continue
+                self.state[p]["old_p"] = p.data.clone()
+                e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
+                p.add_(e_w)  # climb to the local maximum "w + e(w)"
 
-        return x
+        if zero_grad: self.zero_grad()
 
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None: continue
+                p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
 
-def resnet18gai():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+        self.base_optimizer.step()  # do the actual "sharpness-aware" update
 
-def resnet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
+        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
+
+        self.first_step(zero_grad=True)
+        closure()
+        self.second_step()
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+        norm = torch.norm(
+                    torch.stack([
+                        ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                    p=2
+               )
+        return norm
+
+    def load_state_dict(self, state_dict):
+        super().load_state_dict(state_dict)
+        self.base_optimizer.param_groups = self.param_groups
