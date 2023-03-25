d9c7bb72e45016545cd16199f5b5abd1266070ca
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-25 18:46:19 +0800

    Update res.py

--- res.py
+++ res.py
@@ -4,6 +4,59 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+def featureL2Norm(feature):
+    epsilon = 1e-6
+    norm = torch.pow(torch.sum(torch.pow(feature, 2), 1) + epsilon, 0.5).unsqueeze(1).expand_as(feature)
+    return torch.div(feature, norm)
+
+class mySelfCorrelationComputation(nn.Module):
+    def __init__(self,channel, kernel_size=(1, 1), padding=0):
+        super(mySelfCorrelationComputation, self).__init__()
+        planes =[512, 64]
+        self.kernel_size = kernel_size
+        self.unfold = nn.Unfold(kernel_size=kernel_size, padding=padding)
+        self.relu = nn.ReLU(inplace=False)
+
+        self.conv1x1_in = nn.Sequential(nn.Conv2d(planes[0], planes[1], kernel_size=1, bias=False, padding=0),
+                                        nn.BatchNorm2d(planes[1]),
+                                        nn.ReLU(inplace=True))
+        self.embeddingFea = nn.Sequential(nn.Conv2d(channel*2, channel,
+                                                     kernel_size=1, bias=False, padding=0),
+                                           nn.BatchNorm2d(channel),
+                                           nn.ReLU(inplace=True))
+        self.conv1x1_out = nn.Sequential(
+            nn.Conv2d(channel, channel, kernel_size=1, bias=False, padding=0),
+            nn.BatchNorm2d(channel))
+
+    def forward(self, x):
+
+        # x = self.conv1x1_in(x)
+        b, c, h, w = x.shape
+
+        x0 = self.relu(x)
+        x = x0
+        x = F.normalize(x, dim=1, p=2)
+        identity = x
+
+        x = self.unfold(x)  # 提取出滑动的局部区域块，这里滑动窗口大小为5*5，步长为1
+        # b, cuv, h, w  （80,640*5*5,5,5)
+        x = x.view(b, c, self.kernel_size[0], self.kernel_size[1], h, w)  # b, c, u, v, h, w
+        x = x * identity.unsqueeze(2).unsqueeze(2)  # 通过unsqueeze增维使identity和x变为同维度  公式（1）
+        # b, c, u, v, h, w * b, c, 1, 1, h, w
+        x = x.view(b, -1, h, w)
+        # x = x.permute(0, 1, 4, 5, 2, 3).contiguous()  # b, c, h, w, u, v
+        # torch.contiguous()方法首先拷贝了一份张量在内存中的地址，然后将地址按照形状改变后的张量的语义进行排列
+        # x = x.mean(dim=[-1, -2])
+        feature_gs = featureL2Norm(x)
+
+        # concatenate
+        feature_cat = torch.cat([identity, feature_gs], 1)
+
+        # embed
+        feature_embd = self.embeddingFea(feature_cat)
+        feature_embd = self.conv1x1_out(feature_embd)
+        feature_embd = self.relu(feature_embd)
+        return feature_embd
 
 class BasicUnit(nn.Module):
     def __init__(self, channels: int, dropout: float):
@@ -61,17 +114,20 @@ class WideResNet(nn.Module):
 
         self.filters = [16, 1 * 16 * width_factor, 2 * 16 * width_factor, 4 * 16 * width_factor]
         self.block_depth = (depth - 4) // (3 * 2)
+        self.scr_module = mySelfCorrelationComputation(channel=512,kernel_size=(1, 1), padding=0)
 
-        self.f = nn.Sequential(OrderedDict([
+        self.f1 = nn.Sequential(OrderedDict([
             ("0_convolution", nn.Conv2d(in_channels, self.filters[0], (3, 3), stride=1, padding=1, bias=False)),
             ("1_block", Block(self.filters[0], self.filters[1], 1, self.block_depth, dropout)),
             ("2_block", Block(self.filters[1], self.filters[2], 2, self.block_depth, dropout)),
             ("3_block", Block(self.filters[2], self.filters[3], 2, self.block_depth, dropout)),
-            ("4_normalization", nn.BatchNorm2d(self.filters[3])),
-            ("5_activation", nn.ReLU(inplace=True)),
-            ("6_pooling", nn.AvgPool2d(kernel_size=8)),
-            ("7_flattening", nn.Flatten()),
-            ("8_classification", nn.Linear(in_features=self.filters[3], out_features=labels)),
+        ]))
+        self.f2 = nn.Sequential(OrderedDict([
+            ("1_normalization", nn.BatchNorm2d(self.filters[3])),
+            ("2_activation", nn.ReLU(inplace=True)),
+            ("3_pooling", nn.AvgPool2d(kernel_size=8)),
+            ("4_flattening", nn.Flatten()),
+            ("5_classification", nn.Linear(in_features=self.filters[3], out_features=labels)),
         ]))
 
         self._initialize()
@@ -90,67 +146,11 @@ class WideResNet(nn.Module):
                 m.bias.data.zero_()
 
     def forward(self, x):
-        return self.f(x)
+        x = self.f1(x)
+        identity = x
+        x = self.scr_module(x)
 
+        x = x + identity
+        x = self.f2(x)
 
-
-class SAM(torch.optim.Optimizer):
-    def __init__(self, params, base_optimizer, rho=0.05, adaptive=False, **kwargs):
-        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
-
-        defaults = dict(rho=rho, adaptive=adaptive, **kwargs)
-        super(SAM, self).__init__(params, defaults)
-
-        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
-        self.param_groups = self.base_optimizer.param_groups
-        self.defaults.update(self.base_optimizer.defaults)
-
-    @torch.no_grad()
-    def first_step(self, zero_grad=False):
-        grad_norm = self._grad_norm()
-        for group in self.param_groups:
-            scale = group["rho"] / (grad_norm + 1e-12)
-
-            for p in group["params"]:
-                if p.grad is None: continue
-                self.state[p]["old_p"] = p.data.clone()
-                e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
-                p.add_(e_w)  # climb to the local maximum "w + e(w)"
-
-        if zero_grad: self.zero_grad()
-
-    @torch.no_grad()
-    def second_step(self, zero_grad=False):
-        for group in self.param_groups:
-            for p in group["params"]:
-                if p.grad is None: continue
-                p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
-
-        self.base_optimizer.step()  # do the actual "sharpness-aware" update
-
-        if zero_grad: self.zero_grad()
-
-    @torch.no_grad()
-    def step(self, closure=None):
-        assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
-        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
-
-        self.first_step(zero_grad=True)
-        closure()
-        self.second_step()
-
-    def _grad_norm(self):
-        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
-        norm = torch.norm(
-                    torch.stack([
-                        ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
-                        for group in self.param_groups for p in group["params"]
-                        if p.grad is not None
-                    ]),
-                    p=2
-               )
-        return norm
-
-    def load_state_dict(self, state_dict):
-        super().load_state_dict(state_dict)
-        self.base_optimizer.param_groups = self.param_groups
+        return x
